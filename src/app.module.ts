95df200d0bcd5d6dcc94e237fd8155d206fcdc0d
Thitiphong Nimmak <64160048@go.buu.ac.th>
2024-09-17 17:34:19 +0700

    setting order entity dto and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
-import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'order.db',
+      entities: [],
+      synchronize: true,
+    }), OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,17 @@
-export class Order {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Order {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    productId: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    status: string;
+}
+
